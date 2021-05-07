1dfd27d5d2ac2329941036c0791853a7536d5cda
Mike Owino <mike@mikeowino.com>
2021-05-07 09:07:27 +0000

    error

---
+++ Hello C#/Errors.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace BugSquasher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double number = 38498.3222;
+
+      string dinosaur = "Barney";
+
+      double locke = 293.000;
+
+      bool is_yes = true;
+
+      string band = "The Low Anthem";
+
+    }
+  }
+}
+
+
+// error code
+
+using System;
+
+namespace BugSquasher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int number = 38498.3222;
+
+      dinosaur = "Barney";
+
+      double lock = 293.000;
+
+      bool is.yes = true;
+
+      string band = "The Low Anthem"
+
+    }
+  }
+}
